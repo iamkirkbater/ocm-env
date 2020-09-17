1bd7059be6b065adf9d28cde76c04c0728bf468b
Kirk Bater <kirk.bater@gmail.com>
2020-09-17 15:41:44 -0400

    Update init.sh to add symlink

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd $(dirname $0)
+
 CONFIG_DIR=${HOME}/.config/ocm-env
 mkdir -p ${CONFIG_DIR}
 
@@ -11,4 +13,14 @@ else
   echo "ocm-env is already configured."
 fi
 
+echo "Creating symlink"
+ln -s $(pwd)/ocm-env /usr/local/bin/ocm-env
+
+echo
+echo "Tip: Many developers like to add the following alias:"
+echo "alias ocm-env-stg=\"OCM_URL=staging ocm-env\""
+
+echo
+echo
 echo "ocm-env configuration can be customized by editing ${CONFIG_DIR}/env.source"
+
